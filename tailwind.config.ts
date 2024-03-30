8438e3a18a61219663ba51c0666bbfa11624b34f
Maria Warnes <36001658+mariawarnes@users.noreply.github.com>
2024-03-30 08:35:08 +0000

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "Arial", "Helvetica", "sans-serif"],
+      },
+      colors: {
+        black: "#545454",
+        grey: "#808080",
+        "light-grey": "#a4a4a4",
+        yellow: "#ffc20d",
+        "light-green": "#e1ece3",
+        "dark-green": "#006230",
+        blue: "0E82AF",
+        red: "DE3804",
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ]
+};
+export default config;
